00a6a02300b91da846c61204e522f5727e457b61
Yazeed AlKhalaf <yazeed@alkhalaf.dev>
2024-10-06 19:52:16 +0300

    use our domain, just so people don't block it, hopeuflly :D

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -4,7 +4,8 @@ import { browser } from '$app/environment';
 export const load = async () => {
 	if (browser) {
 		posthog.init('phc_j1sPADK2Iw8VHpEo6EDpnwn3hxVms9BSUDljNrYdlZt', {
-			api_host: 'https://us.i.posthog.com',
+			api_host: 'https://pineapple-flucks.yutools.com', // this thing is in cloudflare :D
+			ui_host: 'https://us.posthog.com',
 			person_profiles: 'always'
 		});
 	}
